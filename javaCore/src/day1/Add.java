97b76d6a0154d1f98c3d0b337df78c19bf11a6d5
Admin <Admin@vinoth>
2024-06-27 11:03:36 +0530

    this is my java project

---
+++ javaCore/src/day1/Add.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to JFS");
+	System.out.print("jfs");
+	}
+
+}
