a68d885fdaa9270c0a89250d5e3aa31fa34e0951
Admin <Admin@vinoth>
2024-06-27 12:16:57 +0530

    added some demo program

---
+++ javaCore/src/day1/DataTypeDemo.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int value1 =9/2;
+float value2 = 10l/6l;
+double value3 = 10d/6d;
+float value4 = 5/2.5f;
+System.out.println(" value 1 ="+value1);
+System.out.println(" value 2 ="+value2);
+System.out.println(" value 3 ="+value3);
+System.out.println(" value 4 ="+value4);
+int marker =512;
+double percentage = marker * 0.46f;
+System.out.println(" percentage :"+ percentage);
+	}
+
+}
