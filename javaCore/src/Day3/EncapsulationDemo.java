5a4d577c966a4dd7dbdfa80116f22234a453f939
Admin <Admin@vinoth>
2024-07-03 22:39:56 +0530

    new one

--- javaCore/src/Day3/EncapsulationDemo.java
+++ javaCore/src/Day3/EncapsulationDemo.java
@@ -5,6 +5,11 @@ public class EncapsulationDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-
-}
+		//object creation
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		}
+		}
