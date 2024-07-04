9237f25adfa1102dac11bd27a5b6d687ba81d8bb
Admin <Admin@vinoth>
2024-07-04 14:53:54 +0530

    if else demo programm

--- javaCore/src/Day2/IfDemo1.java
+++ javaCore/src/Day2/IfDemo1.java
@@ -4,9 +4,9 @@ public class IfDemo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 5;
-		if (x != 5)
-		System.out.println("Value of x is not 5");
-		System.out.println("Value of x is 5");
+		int x = 8;
+		if (x != 8)
+		System.out.println("Value of x is not 8");
+		System.out.println("Value of x is 8");
 		}
 		}
