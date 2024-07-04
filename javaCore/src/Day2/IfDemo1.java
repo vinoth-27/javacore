7d291e44339ccc29772d89bbaa30aadf595a91c2
Admin <Admin@vinoth>
2024-07-04 14:46:31 +0530

    added one program in day 2

---
+++ javaCore/src/Day2/IfDemo1.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class IfDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		if (x != 5)
+		System.out.println("Value of x is not 5");
+		System.out.println("Value of x is 5");
+		}
+		}
